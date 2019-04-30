f1e5a1367c2d9dc3f3920eeced22790dd763c874
unax <un@ax>
2019-04-30 14:31:34 -0700

    fix css links

--- update.sh
+++ update.sh
@@ -14,6 +14,9 @@ rename -f -v 's/(.*\.css).*.css/$1/' diamondhunt.co/css/*.css?*
 
 # remove cachebusters from HTML
 sed -i 's/%3Fversion=[0-9]\+//' diamondhunt.co/*.html
+# fix CSS funny names
+sed -i 's/\.css\.html/.css/' diamondhunt.co/*.html
+sed -i 's/\.css\.css/.css/' diamondhunt.co/*.html
 
 # remove players online count to avoid constant updates
 sed -i -r 's/(Players Online.+) [0-9]+/\1/' diamondhunt.co/*.html
